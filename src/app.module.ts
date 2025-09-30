d3413d9269259ef81c370d1e83611fb8bcb05609
qrowan <hsjii1589@gmail.com>
2025-09-30 14:15:44 +0900

    fix: bug

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SS1Module } from './strategies/ss1/ss1.module';
 
 @Module({
-  imports: [ScheduleModule.forRoot()],
+  imports: [ScheduleModule.forRoot(), SS1Module],
   controllers: [AppController],
   providers: [AppService],
 })
